788202caba04b7d8f37bbd3561568551da942ad6
christ <I1-P163@Pinfo1.local>
2026-05-28 10:13:12 +0000

    ajout de la methode bookshelfContainsTwoBooksWhenTwoBooksAdded et mise a jour de la methode add

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -18,7 +18,7 @@ public class BookShelfSpec {
     @Test
     void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
         BookShelf shelf = new BookShelf();
-        shelf.add("Effective Java");
+        shelf.add("Effective Java ");
         shelf.add("Code Complete");
         List<String> books = shelf.books();
         assertEquals(2, books.size(), () -> "BookShelf should have two books.");
