f0bb068447f44cc21f98db3c075d82b4c8f7a981
christ <I1-P163@Pinfo1.local>
2026-06-04 10:58:01 +0000

    Creation de la classe book et ajout d un test permettant le trie des livres par caracteres

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,19 +1,24 @@
 package bookstoread;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookShelf {
-    private final List<String> books = new ArrayList<>();
+    private final List<Book> books = new ArrayList<>();
 
-    public List<String> books() {
-        return Collections.unmodifiableList(books);
+    public List<Book> books() {
+        return  Collections.unmodifiableList(books);
     }
 
-    public void add(String... booksToAdd) {
+    public void add(Book... booksToAdd) {
         books.addAll(Arrays.asList(booksToAdd));
     }
 
-    public List<String> arrange() {
+    public List<Book> arrange() {
         return books.stream().sorted().collect(Collectors.toList());
     }
+    public List<Book> arrange(Comparator<Book> criteria) {
+        return books.stream().sorted(criteria).collect(Collectors.toList());
+    }
+
 }
