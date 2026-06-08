5308882e3953a0f7e8152929384599e8729a6584
mvssy10 <koffiadoumartial7@icloud.com>
2026-06-08 10:58:05 +0000

    amelioration de la methode groupBooksInsideBookShelfByPublicationYear()pour devenir groupBooksByUserProvidedCriteria() permettant de regrouper selon un caractere

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,6 +1,8 @@
 package bookstoread;
 
+import java.time.Year;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BookShelf {
@@ -21,4 +23,12 @@ public class BookShelf {
         return books.stream().sorted(criteria).collect(Collectors.toList());
     }
 
+
+    public Map<Year, List<Book>> groupByPublicationYear() {
+        return  books.stream().collect(Collectors.groupingBy(book ->
+                Year.of(book.getPublishedOn().getYear())));
+    }
+    public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx) {
+        return books.stream().collect(Collectors.groupingBy(fx));
+    }
 }
